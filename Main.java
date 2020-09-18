da029cb7312698455157cede75bb47879e59b854
AustinKavelman <kavea9410@wrdsb.ca>
2020-09-18 14:43:35 +0000

    Finished Sorting the numbers in acending order

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * This program corrects the order of multiple numbers
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Enter line that asks the user to enter an interger
+    System.out.println("Please enter an interger:");
+    int a = input.nextInt();
+
+    //Enter line that asks the user to enter another interger
+    System.out.println("Please enter another interger:");
+    int b = input.nextInt();
+
+    //Sorts the numbers in acending order
+    if (a > b) {
+    System.out.println("Your numbers in ascending order are " + b + ", " + a); 
+    } else if (a < b) {
+    System.out.println("Your numbers in ascending order are " + a + ", " + b);
+    }
   }
 }
